0b4bc9d4d06bd8a6c5c8e3f9266a27b9fe254f28
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-16 15:33:10 -0700

    Val score: 12.575668 | wandb run: jumping-snowball-220

--- src/data_process.py
+++ src/data_process.py
@@ -129,11 +129,11 @@ def preprocess(df):
 
 
 def feature_eng(df, df_desc=None):
-    # # Better capture cyclical nature of day and time
-    # df["Day_sin"] = np.sin(2 * np.pi * df["Publication_Day"] / 7)
-    # df["Day_cos"] = np.cos(2 * np.pi * df["Publication_Day"] / 7)
-    # df["Time_sin"] = np.sin(2 * np.pi * df["Publication_Time"] / 4)
-    # df["Time_cos"] = np.cos(2 * np.pi * df["Publication_Time"] / 4)
+    # Better capture cyclical nature of day and time
+    df["Day_sin"] = np.sin(2 * np.pi * df["Publication_Day"] / 7)
+    df["Day_cos"] = np.cos(2 * np.pi * df["Publication_Day"] / 7)
+    df["Time_sin"] = np.sin(2 * np.pi * df["Publication_Time"] / 4)
+    df["Time_cos"] = np.cos(2 * np.pi * df["Publication_Time"] / 4)
 
     # # Higher frequency sinusoidal features for day and time
     # df["Day_sin2"] = np.sin(4 * np.pi * df["Publication_Day"] / 7)
