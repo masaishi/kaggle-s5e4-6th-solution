fd17d738ed23a5a40ac62ce33c1dc4a7117f92cd
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-19 04:44:45 -0700

    Val score: 12.590077 | wandb run: wobbly-fire-284

--- src/data_process.py
+++ src/data_process.py
@@ -137,11 +137,11 @@ def feature_eng(df, df_train):
 
     pwg_mean = df_train.groupby(["Podcast_Name", "Host_Popularity_percentage"])[["Listening_Time_minutes"]].mean()
     pwg_mean_dict = pwg_mean["Listening_Time_minutes"].to_dict()
-    df["Podcast_Host_Guest_mean_Listening_Time"] = df[["Podcast_Name", "Host_Popularity_percentage", "Guest_Popularity_percentage"]].apply(
+    df["Podcast_Host_Guest_mean_Listening_Time"] = df[["Podcast_Name", "Host_Popularity_percentage"]].apply(
         lambda x: pwg_mean_dict.get((x["Podcast_Name"], x["Host_Popularity_percentage"]), pwg_mean["Listening_Time_minutes"].mean()), axis=1
     )
     df["Podcast_Host_Guest_combination_exists"] = df.apply(
-        lambda row: 1 if (row["Podcast_Name"], row["Host_Popularity_percentage"], row["Guest_Popularity_percentage"]) in pwg_mean_dict else 0, axis=1
+        lambda row: 1 if (row["Podcast_Name"], row["Host_Popularity_percentage"]) in pwg_mean_dict else 0, axis=1
     )
 
     df["Podcast_Name"] = df["Podcast_Name"].astype("category")
