f3898e7367374779b4661a5bceb0dc555b806615
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-19 13:59:52 -0700

    Val score: 12.208712 | wandb run: efficient-sky-287

--- src/data_process.py
+++ src/data_process.py
@@ -245,27 +245,27 @@ def get_dfs(cfg=cfg):
     X_valid = feature_eng(X_valid, df_train)
     X_test = feature_eng(X_test, df_train)
 
-    # before_encode_len = len(X_train.columns)
-    # print("Length of train columns:", before_encode_len)
+    before_encode_len = len(X_train.columns)
+    print("Length of train columns:", before_encode_len)
 
-    # X_train = cols_encode(X_train)
-    # X_valid = cols_encode(X_valid)
-    # X_test = cols_encode(X_test)
+    X_train = cols_encode(X_train)
+    X_valid = cols_encode(X_valid)
+    X_test = cols_encode(X_test)
 
-    # X_test = X_test[X_train.columns].copy()
+    X_test = X_test[X_train.columns].copy()
 
-    # print(X_train.shape, y_train.shape, X_valid.shape, y_valid.shape)
+    print(X_train.shape, y_train.shape, X_valid.shape, y_valid.shape)
 
-    # # Target encoding
-    # print("Before encoding columns:", before_encode_len)
-    # encoded_columns = X_train.columns[before_encode_len:]
+    # Target encoding
+    print("Before encoding columns:", before_encode_len)
+    encoded_columns = X_train.columns[before_encode_len:]
 
-    # from sklearn.preprocessing import TargetEncoder
+    from sklearn.preprocessing import TargetEncoder
 
-    # encoder = TargetEncoder(random_state=cfg.random_state)
-    # X_train[encoded_columns] = encoder.fit_transform(X_train[encoded_columns], y_train)
-    # X_valid[encoded_columns] = encoder.transform(X_valid[encoded_columns])
-    # X_test[encoded_columns] = encoder.transform(X_test[encoded_columns])
+    encoder = TargetEncoder(random_state=cfg.random_state)
+    X_train[encoded_columns] = encoder.fit_transform(X_train[encoded_columns], y_train)
+    X_valid[encoded_columns] = encoder.transform(X_valid[encoded_columns])
+    X_test[encoded_columns] = encoder.transform(X_test[encoded_columns])
 
     return {
         "X_train": X_train,
