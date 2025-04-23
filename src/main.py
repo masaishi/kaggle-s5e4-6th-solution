ed603205d04c0b5c91dc710510c9176554159a0e
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-23 16:52:55 -0700

    Val score: 12.682941 | wandb run: dazzling-eon-590

--- src/main.py
+++ src/main.py
@@ -12,9 +12,9 @@ import wandb
 from config import cfg
 from data.data_class import Dfs
 from data.data_process import add_fold, get_Xy
+from models.lgb import train_model
 
-# from models.lgb import train_model
-from models.xgb import train_model
+# from models.xgb import train_model
 from utils import commit_results
 
 warnings.filterwarnings("ignore")
