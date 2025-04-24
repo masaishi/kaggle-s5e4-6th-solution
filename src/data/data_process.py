16a4f5f50fa017a1dcf3df16f54ab51b91822ae3
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-23 18:46:42 -0700

    Val score: 12.316033 | wandb run: noble-mountain-602

--- src/data/data_process.py
+++ src/data/data_process.py
@@ -58,7 +58,7 @@ def get_Xy(dfs: Dfs) -> DatasetXy:
     if X_test is not None:
         X_test = add_original_cols(X_test)
 
-    # datasetX = add_te(y_train, X_train, X_valid, X_test)
-    # X_train, X_valid, X_test = datasetX.get()
+    datasetX = add_te(y_train, X_train, X_valid, X_test)
+    X_train, X_valid, X_test = datasetX.get()
 
     return DatasetXy(X_train=X_train, y_train=y_train, X_valid=X_valid, y_valid=y_valid, X_test=X_test, y_test=None)
