efa2188fad60ff376cf5cd1392360cb2dbe1bf6e
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-23 21:52:34 -0700

    Val score: 12.320298 | wandb run: whole-monkey-613

--- src/data/feature_eng.py
+++ src/data/feature_eng.py
@@ -205,9 +205,28 @@ def feature_eng(df: pl.DataFrame, df_train: pl.DataFrame) -> pl.DataFrame:
         (pl.col("Episode_Length_minutes") * pl.col("Sentiment_Multiplier")).alias("Expected_Listening_Time_Sentiment"),
     )
 
+    combinations_list = [
+        "Episode_Num-Length_per_Guest",
+        "Length_per_Guest-HPperc_Dec",
+        "Length_per_Guest-HPperc_Int",
+        "Length_per_Guest-Long_Term_Cycle_Cos",
+        "Episode_Num-Length_per_Host",
+        "Length_per_Guest-Long_Term_Cycle_Sin",
+        "Length_per_Host-Long_Term_Cycle_Cos",
+        "Length_per_Host-Long_Term_Cycle_Sin",
+        "Episode_Length_minutes-Length_per_Host",
+        "Episode_Length_minutes-Host_Popularity_percentage",
+        "Host_Popularity_percentage-Length_per_Host",
+        "Length_per_Host-ELen_Int",
+        "Length_per_Host-HPperc_Int",
+        "Length_per_Host-HPperc_Dec",
+        "Length_per_Host-ELen_Dec",
+        "Length_per_Host-Length_per_Guest",
+    ]
     interaction_transforms = []
-
-    for comb in selecteds:
+    for comb in combinations_list:
+        if "-" not in comb:
+            continue
         col1, col2 = comb.split("-")
 
         interaction_transforms.append(pl.max_horizontal(pl.col(col1), pl.col(col2)).alias(f"{col1}_{col2}_max"))
