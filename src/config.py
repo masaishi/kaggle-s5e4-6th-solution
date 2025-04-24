6582dcce96959783151dcba72b74d7a71c40ef2d
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-24 04:21:35 -0700

    Val score: 12.085458 | wandb run: kind-flower-705

--- src/config.py
+++ src/config.py
@@ -34,8 +34,8 @@ class CFG:
     default_combinations: bool = True
 
     # debug: bool = True
-    eval: bool = True
-    # predict: bool = True
+    # eval: bool = True
+    predict: bool = True
 
     device: str = "cpu"
 
