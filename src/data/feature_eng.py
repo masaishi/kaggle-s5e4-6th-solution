589326384dea7066fe5d2fb303e22c569c2624d1
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-26 04:28:58 -0700

    Val score: 12.262532 | wandb run: woven-sky-815

--- src/data/feature_eng.py
+++ src/data/feature_eng.py
@@ -813,7 +813,7 @@ def add_original_cols(df: pl.DataFrame, df_pltpd: pl.DataFrame) -> pl.DataFrame:
 
     combinations_list = []
     combinations_list += [item.split("-") for item in before_fe_selecteds]
-    combinations_list += [[col] for col in numeric_cols]
+    # combinations_list += [[col] for col in numeric_cols]
 
     m = df_pltpd["Listening_Time_minutes"].mean()
 
