a57af0b872590d4660319ff29f592ab6c41fff48
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-25 19:48:11 -0700

    Val score: 12.321790 | wandb run: peach-surf-768

--- src/data/feature_eng.py
+++ src/data/feature_eng.py
@@ -801,10 +801,10 @@ def add_original_cols(df: pl.DataFrame, df_pltpd: pl.DataFrame) -> pl.DataFrame:
     if "id" in numeric_cols:
         numeric_cols.remove("id")
 
-    # combinations_list = [[col] for col in numeric_cols] + default_combinations_list
+    combinations_list = [[col] for col in numeric_cols] + random.sample(default_combinations_list, 20)
 
-    combinations_list = [item.split("-") for item in before_fe_selecteds]
-    combinations_list += [[col] for col in numeric_cols]
+    # combinations_list = [item.split("-") for item in before_fe_selecteds]
+    # combinations_list += [[col] for col in numeric_cols]
 
     m = df_pltpd["Listening_Time_minutes"].mean()
 
