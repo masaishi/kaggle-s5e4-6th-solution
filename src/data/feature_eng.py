789e7def7fa11a05608a11305f9ec406da9124d4
Masamune William Ishihara <mwishiha@ucsc.edu>
2025-04-28 18:19:26 -0700

    Val score: 12.202680 | wandb run: hopeful-planet-871

--- src/data/feature_eng.py
+++ src/data/feature_eng.py
@@ -519,9 +519,9 @@ def preprocess(df: pl.DataFrame, df_train: pl.DataFrame = None) -> pl.DataFrame:
     # n_median = df_train.select(pl.col("Number_of_Ads").median()).item()
 
     df = df.with_columns(
-        pl.col("Episode_Length_minutes").fill_null(-1.1),
-        pl.col("Guest_Popularity_percentage").fill_null(-1.1),
-        pl.col("Number_of_Ads").fill_null(-1.1),
+        pl.col("Episode_Length_minutes").fill_null(-2),
+        pl.col("Guest_Popularity_percentage").fill_null(-2),
+        pl.col("Number_of_Ads").fill_null(-2),
     )
 
     return df
